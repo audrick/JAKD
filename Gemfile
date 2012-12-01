602a105034b25559978d3ed7e0002a48e7750852
Audric Kim <audrickim@gmail.com>
2012-12-01 12:02:32 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.2.9'
+
+gem 'jquery-rails'
+gem 'pg'
+gem 'authlogic'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'thin'
+end
+
+group :development do
+  gem 'annotate'
+  gem 'rails-footnotes'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jakd::Application.config.secret_token = '13ec25cfe8a50640a1d158833ff0a48a35039b21baf87f022967e4cf8b9b491a680a7133a28740bd8f979978171492be3d9fa772826fe1f9853b1a76a039fca4'
