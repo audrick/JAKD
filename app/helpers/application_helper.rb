cc106dbe1958891cc468c30377b9fdbb7a910183
Derrick Lannaman <derricklannaman@gmail.com>
2012-12-01 14:49:31 -0500

    add signed in? method to App-helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def signedin?
+    if @authenticated_user
+      link_to "#{@authenticated_user.name}, Sign Out Here", signout_path
+    else
+      link_to "Sign In", signin_path
+    end
+  end
 end
